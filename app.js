f883725688ce548928dc7f8d106184e4766cc9b8
yatharth <yatharthverma070@gmail.com>
2020-06-02 14:14:34 +0530

    Fix bugs

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Expose-Headers", "x-access-token, x-refresh-token");
     next();
 });
 
