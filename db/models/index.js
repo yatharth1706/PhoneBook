a98fc218a28bbefb9026482967bf53b211ba1713
yatharth <yatharthverma070@gmail.com>
2020-06-02 13:05:25 +0530

    feat: Add Instance methods in user Model :hammer:

--- db/models/index.js
+++ db/models/index.js
@@ -1,5 +1,7 @@
 const { ContactDetails } = require('./contactDetails.model');
+const { User } = require('./user.model');
 
 module.exports = {
-    ContactDetails
+    ContactDetails,
+    User
 }
--- db/models/user.model.js
+++ db/models/user.model.js
@@ -2,108 +2,166 @@ const mongoose = require('mongoose');
 const _ = require('lodash');
 const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
+const bcrypt = require('bcryptjs');
 // create user schema
 
 // JWT secret
 const jwtSecret = "2983498273984789273947890198238129";
 
-const UserSchema = new monoose.Schema({
-    email : {
+const UserSchema = new mongoose.Schema({
+    email: {
         type: String,
-        trim: true,
-        minlength : 1,
         required: true,
+        minlength: 1,
+        trim: true,
         unique: true
     },
     password: {
         type: String,
         required: true,
-        minlength : 8
+        minlength: 8
     },
     sessions: [{
         token: {
             type: String,
             required: true
         },
-        expiresAt : {
-            type : Number,
+        expiresAt: {
+            type: Number,
             required: true
         }
     }]
-})
+});
 
-// *** Instance Methods *** //
 
-// 1st Instance method
-// Purpose: To overwrite toJson method of this schema and return only email and 
-// omit password and sessions
+// *** Instance methods ***
 
-UserSchema.methods.toJson = function() {
+UserSchema.methods.toJSON = function () {
     const user = this;
     const userObject = user.toObject();
 
-    // return the document except password and sessions .. for that we need lodash lib
-    return _.omit(userObject, ['password','sessions']);
-
+    // return the document except the password and sessions (these shouldn't be made available)
+    return _.omit(userObject, ['password', 'sessions']);
 }
 
-// 2nd instance method
-// Purpose: generate access token
-
-UserSchema.methods.generateAccessAuthToken = function() {
+UserSchema.methods.generateAccessAuthToken = function () {
     const user = this;
     return new Promise((resolve, reject) => {
-        // create a JSON web token and return it
-        jwt.sign({ _id: user._id.toHexString() }, jwtSecret, {expiresIn: "15m"}, (e,token) => {
-            if(!e){
-                resolve(token)
-            }else{
-                // error
+        // Create the JSON Web Token and return that
+        jwt.sign({ _id: user._id.toHexString() }, jwtSecret, { expiresIn: "15m" }, (err, token) => {
+            if (!err) {
+                resolve(token);
+            } else {
+                // there is an error
                 reject();
             }
         })
     })
 }
 
-
-// 3nd instance method
-// Purpose: generate Refresh access token
-
-UserSchema.methods.generateRefreshAccessAuthToken = function() {
-    const user = this;
+UserSchema.methods.generateRefreshAuthToken = function () {
+    // This method simply generates a 64byte hex string - it doesn't save it to the database. saveSessionToDatabase() does that.
     return new Promise((resolve, reject) => {
-        crypto.randomBytes(64, (err, buffer) => {
-            if(!err){
+        crypto.randomBytes(64, (err, buf) => {
+            if (!err) {
+                // no error
                 let token = buf.toString('hex');
 
                 return resolve(token);
             }
-        })       
-        
+        })
     })
 }
 
-// 4rth instance method
-// Purpose: create sessions
+UserSchema.methods.createSession = function () {
+    let user = this;
 
-UserSchema.methods.createSession = function() {
-    const user = this;
-    
-    return user.generateRefreshAccessAuthToken().then((refreshToken) => {
-        return saveSessionDatabase(user, refreshToken);
+    return user.generateRefreshAuthToken().then((refreshToken) => {
+        return saveSessionToDatabase(user, refreshToken);
     }).then((refreshToken) => {
-        // saved to database successfully 
-        // now return refreshToken
-        return refreshToken
+        // saved to database successfully
+        // now return the refresh token
+        return refreshToken;
     }).catch((e) => {
-        return Promise.reject("Failed to save session to database!! \n" + e);
+        return Promise.reject('Failed to save session to database.\n' + e);
     })
 }
 
 
-// ** Helper Methods ** //
 
-let saveSessionDatabase = (user, refreshToken) => {
+/* MODEL METHODS (static methods) */
+
+UserSchema.statics.getJWTSecret = () => {
+    return jwtSecret;
+}
+
+
+
+UserSchema.statics.findByIdAndToken = function (_id, token) {
+    // finds user by id and token
+    // used in auth middleware (verifySession)
+
+    const User = this;
+
+    return User.findOne({
+        _id,
+        'sessions.token': token
+    });
+}
+
+
+UserSchema.statics.findByCredentials = function (email, password) {
+    let User = this;
+    return User.findOne({ email }).then((user) => {
+        if (!user) return Promise.reject();
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res) {
+                    resolve(user);
+                }
+                else {
+                    reject();
+                }
+            })
+        })
+    })
+}
+
+UserSchema.statics.hasRefreshTokenExpired = (expiresAt) => {
+    let secondsSinceEpoch = Date.now() / 1000;
+    if (expiresAt > secondsSinceEpoch) {
+        // hasn't expired
+        return false;
+    } else {
+        // has expired
+        return true;
+    }
+}
+/* MIDDLEWARE */
+
+// Before a user document is saved, this code runs
+
+UserSchema.pre('save', function (next) {
+    const user = this;
+    const costFactor = 10;
+
+    if(user.isModified('password')){
+        // if password field is modified/ edited then run this code
+
+        // generate salt and hash password
+        bcrypt.genSalt(costFactor, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            })
+        })
+    }else{
+        next();
+    }
+})
+
+let saveSessionToDatabase = (user, refreshToken) => {
     // save session to database
     return new Promise((resolve, reject) => {
         let expiresAt = generateRefreshTokenExpiryTime();
@@ -125,25 +183,11 @@ let saveSessionDatabase = (user, refreshToken) => {
 let generateRefreshTokenExpiryTime = () => {
     let daysUntilExpire = "10";
     let secondsUntilExpire = ((daysUntilExpire * 24) * 60 ) * 60;
-    return (Date.now() / 1000) + secondsUntilExpire);
+    return ((Date.now() / 1000) + secondsUntilExpire);
 } 
 
+const User = mongoose.model('User', UserSchema);
 
-// ** Modal Methods (Static Methods) ** //
-
-UserSchema.statics.findIdAndToken = (_id,token) => {
-    // find user by id and token
-    // used in auth middleware (verifySession)
-    const user = this;
-
-    return user.findOne({
-        _id,
-        'session.token':token
-    })
-}
-
-UserSchema.statics.findByCredentials = (email, password) => {
-    const user = this;
-
-    // we will create one middleware to decrypt and encryt our password first
+module.exports = {
+    User
 }
